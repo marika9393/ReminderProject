b431ea775902d2b0505bbc253888f68e58a9f5e5
marika9393 <marika-piotrowska@wp.pl>
2020-08-18 22:20:05 +0200

    Utworzenie klasy konfiguracyjnej hibernate.cfg.xml oraz EntityDao i HibernateUtil

--- src/main/java/ReminderProject/Main.java
+++ src/main/java/ReminderProject/Main.java
@@ -1,8 +1,25 @@
 package ReminderProject;
 
+import ReminderProject.database.EntityDao;
+import ReminderProject.database.HibernateUtil;
+import ReminderProject.model.Employee;
+import ReminderProject.model.TypeOfContract;
+import ReminderProject.model.TypeOfReminder;
+
+import javax.swing.text.html.parser.Entity;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Initial version");
+        HibernateUtil.getOurSessionFactory();
+        System.out.println("Tested hibernate");
+
+        Employee FirstEmployee = new Employee("Mariolka", "Blask");
+
+        EntityDao<Employee> employeeEntityDao = new EntityDao<>();
+        employeeEntityDao.saveOrUpdate(FirstEmployee);
     }
 }
---
+++ src/main/java/ReminderProject/database/HibernateUtil.java
@@ -0,0 +1,30 @@
+package ReminderProject.database;
+
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            System.out.println("Konfiguruję hibernate.");
+
+
+            Configuration configuration = new Configuration();
+            configuration.configure("/hibernate.cfg.xml");
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        }catch (HibernateException he){
+            System.err.println(he.getMessage());
+            throw he;
+        }
+    }
+
+
+    public static SessionFactory getOurSessionFactory() {
+        return ourSessionFactory;
+    }
+}
